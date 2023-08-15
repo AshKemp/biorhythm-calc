a8d0e30679e09a09a7fadf2f74db1163b039e91e
AshKemp <kmuraliashwin@gmail.com>
2023-08-15 09:46:20 +0530

    Impl biorhythm card component

--- src/components/BiorhythmCard.js
+++ src/components/BiorhythmCard.js
@@ -5,21 +5,31 @@ import {
   IonCardTitle,
 } from "@ionic/react";
 import dayjs from "dayjs";
+import { calculateBiorhythms } from "../lib/biorhythms";
+import BiorhythmChart from "./BiorhythmChart";
+import "./BiorhythmCard.css";
 
 function formatDate(isoString) {
   const day = dayjs(isoString);
   return day.format("D MMMM YYYY");
 }
-function BiorhythmCard({ targetDate }) {
+function BiorhythmCard({ birthDate, targetDate }) {
+  const biorhythms = calculateBiorhythms(birthDate, targetDate);
+
   return (
-    <IonCard class="ion-text-center">
+    <IonCard class="BiorhythmCard ion-text-center">
       <IonCardHeader>
         <IonCardTitle>{formatDate(targetDate)}</IonCardTitle>
       </IonCardHeader>
       <IonCardContent>
-        <p>Physical:</p>
-        <p>Emotional:</p>
-        <p>Intellectual:</p>
+        <BiorhythmChart birthDate={birthDate} targetDate={targetDate} />
+        <p className="physical">Physical: {biorhythms.physical.toFixed(4)}</p>
+        <p className="emotional">
+          Emotional: {biorhythms.emotional.toFixed(4)}
+        </p>
+        <p className="intellectual">
+          Intellectual: {biorhythms.intellectual.toFixed(4)}
+        </p>
       </IonCardContent>
     </IonCard>
   );

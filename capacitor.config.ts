6c1b6f8827b5f54e882dfb8faf1a2c4a6d83c64a
AshKemp <kmuraliashwin@gmail.com>
2023-10-02 16:58:59 +0530

    update capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'org.example.biorhythms',
-  appName: 'biorhythm-calculator',
-  webDir: 'build',
+  appId: "org.example.biorhythms",
+  appName: "biorhythm-calculator",
+  webDir: "build",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
